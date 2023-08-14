45ba94f1bf896cdee1cb8b329edc39b077a19ee2
kjgit2412!! <idiskkj@hanmail.net>
2023-08-14 09:33:34 +0900

    SBoard 스프링부트 설정 완료

---
+++ src/test/java/org/koreadit/SBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreadit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
